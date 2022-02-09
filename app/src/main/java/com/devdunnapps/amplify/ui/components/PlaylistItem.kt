741c07a879d56088946044790abda15e08605435
Blake Dunn <BlakeHockey5@gmail.com>
2022-02-09 17:18:40 -0600

    version updates

--- app/src/main/java/com/devdunnapps/amplify/ui/components/PlaylistItem.kt
+++ app/src/main/java/com/devdunnapps/amplify/ui/components/PlaylistItem.kt
@@ -15,8 +15,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
