0bdb275a5156ba9506d4b5b614c41ab99dd30f2c
Blake Dunn <BlakeHockey5@gmail.com>
2023-10-16 00:24:29 -0500

    Update dependencies

--- app/src/main/java/com/devdunnapps/amplify/utils/Resource.kt
+++ app/src/main/java/com/devdunnapps/amplify/utils/Resource.kt
@@ -1,7 +1,7 @@
 package com.devdunnapps.amplify.utils
 
 sealed class Resource<out T> {
-    object Loading : Resource<Nothing>()
+    data object Loading : Resource<Nothing>()
     data class Success<T>(val data: T): Resource<T>()
     data class Error<T>(val message: String? = null, val data: Any? = null): Resource<T>()
 }
